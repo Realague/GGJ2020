c2efdcb7f037c9f57e8d76bdd40a5ff826fd4c44
jjlebow@gmail.com <jlebow@uci.edu>
2020-02-01 15:43:16 -0800

    prefabs folder

--- New Unity Project/Assets/MapScripts/MapInteractables.cs
+++ New Unity Project/Assets/MapScripts/MapInteractables.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MapInteractables : MonoBehaviour
 {
+    private IsometricPlayer player;
     // Start is called before the first frame update
     void Start()
     {
