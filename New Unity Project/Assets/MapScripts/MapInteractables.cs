6c46d678878b9a02d7b9bf2fa4e2932b9333d07f
jjlebow@gmail.com <jlebow@uci.edu>
2020-02-01 16:09:36 -0800

    interactable objects will be highlighted when collided with and are fully interactable

--- New Unity Project/Assets/MapScripts/MapInteractables.cs
+++ New Unity Project/Assets/MapScripts/MapInteractables.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class MapInteractables : MonoBehaviour
 {
     private IsometricPlayer player;
+    private SpriteRenderer m_SpriteRenderer;
+    [SerializeField] Sprite highlightedSprite;
+    private Sprite originalSprite;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        player = gameObject.GetComponent<IsometricPlayer>();
+        originalSprite = m_SpriteRenderer.sprite;
     }
 
     // Update is called once per frame
@@ -18,10 +23,14 @@ public class MapInteractables : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
+        if(col.gameObject.CompareTag("Player"))
+            m_SpriteRenderer.sprite = highlightedSprite;
         //have it turn off the "highlighted" animation here
     }
     private void OnTriggerExit2D(Collider2D col)
     {
+        if(col.gameObject.CompareTag("Player"))
+            m_SpriteRenderer.sprite = originalSprite;
         //have it tur on the "highlighted" animation here
     }
 }
