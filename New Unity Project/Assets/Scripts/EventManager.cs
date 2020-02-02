a3c0e9dd3c5a74622aa7fb30aca0d5abbfa79577
eriejar <michael16574@gmail.com>
2020-02-01 17:13:50 -0800

    5:13

---
+++ New Unity Project/Assets/Scripts/EventManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void ExecuteEvent(string eventName) // user calls event A over B
+    {
+        switch (eventName)
+        {
+            case "event1":
+                event1();
+                break;
+            default:
+                Debug.Log("Invalid Event Called");
+                break;
+
+        }
+    }
+
+
+    void event1()
+    {
+        // worldSculptor.create(0, 0, 0, trees);
+    }
+
+    void StartingEvent()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
